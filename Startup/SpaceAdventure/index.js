7590410802ef3a5276ee7419d1dd5399b2cb3986
“Scotlyn <st699@byu.edu>
2024-03-11 13:06:25 -0600

    added 3rd party quotes in login page

--- Startup/SpaceAdventure/index.js
+++ Startup/SpaceAdventure/index.js
@@ -16,9 +16,37 @@ app.use(`/api`, apiRouter);
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
+  res.sendFile('index.html', { root: 'startup/Startup/SpaceAdventure/public' });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// GetAchievements
+apiRouter.get('/achievements', (_req, res) => {
+  res.send(achievements);
+});
+
+// SubmitAchievement
+apiRouter.post('/achievements', (req, res) => {
+  scores = updateAchievements(req.body, achievements);
+  res.send(achievements);
+});
+
+// updateAchievements considers a new achievement for inclusion in the achievements.
+// The achievements are saved in memory and disappear whenever the service is restarted.
+let achievements = [];
+function updateAchievements(newAchievement, achievements) {
+  let found = false;
+  for (const [i, prevAchievement] of achievements.entries()) {
+      achievements.splice(i, 0, newAchievement);
+      found = true;
+  }
+
+  if (!found) {
+    achievements.push(newAchievement);
+  }
+
+  return achievements;
+}
--- Startup/SpaceAdventure/public/achievements.js
+++ Startup/SpaceAdventure/public/achievements.js
@@ -1,13 +1,22 @@
-function loadAchievements(userName) {
+async function loadAchievements() {
     let achievements = [];
-    const achievementText = localStorage.getItem("achievements");
-    if (achievementText) {
       try {
-        achievements = JSON.parse(achievementText);
+         // Get the latest high scores from the service
+        const response = await fetch('/api/achievements');
+        scores = await response.json();
+
+         // Save the scores in case we go offline in the future
+        localStorage.setItem('achievements', JSON.stringify(achievements));
       } catch (error) {
+        const achievementsText = localStorage.getItem('achievements');
+        if (achievementsText) {
+           achievements = JSON.parse(achievementsText);
+        }
         console.error("Error parsing achievements:", error);
       }
-    }
+}
+
+function displayUserAchievements() {
   
     const tableBodyEl = document.querySelector("#achievements");
     //const userAchievements = achievements;
@@ -31,9 +40,32 @@ function loadAchievements(userName) {
     } else {
       tableBodyEl.innerHTML = '<tr><td colspan="3">Follow the story to get an achievement</td></tr>';
     }
-  }
+}
 
   var userName = localStorage.getItem("userName");
   loadAchievements(userName);
 
 
+function displayAchievements() {
+  
+  const tableBodyEl = document.querySelector("#achievements");
+  //const userAchievements = achievements;
+
+  if (userAchievements.length) {
+    for (const achievement of userAchievements) {
+      const endingTdEl = document.createElement('td');
+      const dateTdEl = document.createElement('td');
+
+      endingTdEl.textContent = achievement.achievement;
+      dateTdEl.textContent = achievement.date;
+
+      const rowEl = document.createElement('tr');
+      rowEl.appendChild(endingTdEl);
+      rowEl.appendChild(dateTdEl);
+
+      tableBodyEl.appendChild(rowEl);
+    }
+  } else {
+    tableBodyEl.innerHTML = '<tr><td colspan="3">Follow the story to get an achievement</td></tr>';
+  }
+}
