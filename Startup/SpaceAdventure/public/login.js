3ce99019205dd882c7126a80965d2355534bad4e
“Scotlyn <st699@byu.edu>
2024-03-16 21:28:32 -0600

    update login.js with login specifics

--- Startup/SpaceAdventure/public/login.js
+++ Startup/SpaceAdventure/public/login.js
@@ -1,7 +1,72 @@
-function login() {
+(async () => {
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    document.querySelector('#playerName').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
   const username = document.querySelector("#userName").value;
-  localStorage.setItem("username", username);
   const password = document.querySelector("#password").value;
-  localStorage.setItem("password", password);
-  window.location.href = "home.html";
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+
+  if (response.ok) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'home.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
+
+function home() {
+  window.location.href = 'home.html';
+}
+
+function logout() {
+  localStorage.removeItem('userName');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(username) {
+  let achievements = [];
+  // See if we have a user with the given username.
+  const response = await fetch(`/api/user/${username}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
 }
